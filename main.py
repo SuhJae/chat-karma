6a20e15e8b036e8d73865e8a468326af7a7b15e1
wonyul <jwsuh25@kis.ac>
2022-10-27 09:00:53 +0900

    Added basic bot

---
+++ main.py
@@ -0,0 +1,103 @@
+# imports
+import configparser
+import platform
+import time
+
+import nextcord
+import redis
+from nextcord import Interaction, Locale
+from nextcord.ext import commands
+
+# load config & language
+config = configparser.ConfigParser()
+config.read('config.ini')
+fallback_lang = configparser.ConfigParser()
+fallback_lang.read('language.ini')
+english = configparser.ConfigParser()
+english.read('language/en_us.ini')
+korean = configparser.ConfigParser()
+korean.read('language/ko_kr.ini')
+chinese = configparser.ConfigParser()
+chinese.read('language/zh_cn.ini')
+
+token = config['CREDENTIALS']['token']
+owner_id = str(config['CREDENTIALS']['owner_id'])
+prefix = config['SETTINGS']['prefix']
+status = config['SETTINGS']['status']
+status_message = config['SETTINGS']['status_message']
+status_type = config['SETTINGS']['status_type']
+host = config['REDIS']['host']
+port = config['REDIS']['port']
+password = config['REDIS']['password']
+db = config['REDIS']['db']
+
+# check config
+error_count = 0
+
+TESTING_GUILD_ID = 1023440388352114749
+
+
+if len(prefix) > 1:
+    print('Error: Prefix must be only one character.')
+    error_count += 1
+
+if status not in ['online', 'idle', 'dnd', 'invisible']:
+    print('Error: Status must be one of online, idle, dnd, or invisible.')
+    error_count += 1
+
+if status_type not in ['playing', 'streaming', 'listening', 'watching']:
+    print('Error: Status type must be one of playing, streaming, listening, or watching.')
+    error_count += 1
+
+if len(status_message) > 128:
+    print('Error: Status message must be less than 128 characters.')
+    error_count += 1
+
+if error_count > 0:
+    print('Please change the config file (config.ini) and try again.')
+    print('Exiting in 5 seconds...')
+    time.sleep(5)
+    exit()
+
+# check redis connection
+try:
+    print(f'Connecting to Redis... ({host}:{port} Database: {db})')
+    r = redis.Redis(host=host, port=port, password=password, decode_responses=True, db=db)
+    r.ping()
+    print(f"Connected to redis.")
+except:
+    print('Error: Could not connect to Redis server.')
+    print('Please change the config file (config.ini) and try again.')
+    print('Exiting in 5 seconds...')
+    time.sleep(5)
+    exit()
+
+# discord setup
+intents = nextcord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+client = commands.Bot(command_prefix=prefix, intents=intents)
+
+# Bot startup
+@client.event
+async def on_ready():
+    # set status
+    if status_type == 'playing':
+        await client.change_presence(activity=nextcord.Game(name=status_message), status=status)
+    elif status_type == 'streaming':
+        await client.change_presence(activity=nextcord.Streaming(name=status_message, url='https://twich.tv'),
+                                     status=status)
+    elif status_type == 'listening':
+        await client.change_presence(
+            activity=nextcord.Activity(type=nextcord.ActivityType.listening, name=status_message), status=status)
+    elif status_type == 'watching':
+        await client.change_presence(
+            activity=nextcord.Activity(type=nextcord.ActivityType.watching, name=status_message), status=status)
+    # print startup message
+    owner_name = await client.fetch_user(owner_id)
+    print('======================================')
+    print(f'Logged in as {client.user.name}#{client.user.discriminator} ({client.user.id})')
+    print(f"Owner: {owner_name} ({owner_id})")
+    print(f'Currenly running nextcord {nextcord.__version__} on python {platform.python_version()}')
+    print('======================================')
